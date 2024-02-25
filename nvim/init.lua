0c893165818fe47aaf53514cf246790225a4b409
Lawrence Wong <lawrencedrums@gmail.com>
2024-02-25 17:19:06 +0000

    lazy: cmp, lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,12 +1,15 @@
+require('options')
+
+
 -- install lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -14,26 +17,55 @@ vim.opt.rtp:prepend(lazypath)
 
 
 -- Your plugins
-require("lazy").setup({
-  -- themes
+require('lazy').setup({
+  -- theme
   {
     'projekt0n/github-nvim-theme',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
   },
 
+  -- lsp
+  'neovim/nvim-lspconfig',
+  'williamboman/mason.nvim',
+  'williamboman/mason-lspconfig.nvim',
+
+  -- auto-complete
+  {
+    'hrsh7th/nvim-cmp',
+    dependencies = {
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'saadparwaiz1/cmp_luasnip',
+    },
+  },
+
+  -- Snippets
+  'L3MON4D3/LuaSnip',
+  'rafamadriz/friendly-snippets',
+
+  -- fuzzy finder
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+
   -- file tree
-  "nvim-tree/nvim-tree.lua",
-  "nvim-tree/nvim-web-devicons",
+  'nvim-tree/nvim-tree.lua',
+  'nvim-tree/nvim-web-devicons',
 
   -- key map hints
-  "folke/which-key.nvim",
+  'folke/which-key.nvim',
 })
 
 
--- load your lua files
+-- load plugin lua files
 require('colorscheme')
-require('options')
 
+require('plugins.lsp')
+require('plugins.nvim-cmp')
 require('plugins.nvim-tree')
+require('plugins.telescope')
 require('plugins.which-key')
--- nvim/lua/plugins/which-key.lua
+++ nvim/lua/plugins/which-key.lua
@@ -1,6 +1,18 @@
+local ts_builtin = require('telescope.builtin')
 local wk = require('which-key')
 
 local mappings = {
+    -- telescope
+    ["<leader>ff"] = { ts_builtin.find_files, "Telescope: find file" },
+    ["<leader>fn"] = { ts_builtin.grep_string, "Telescope: grep sting under cursor" },
+    ["<leader>fp"] = {
+        function()
+            ts_builtin.grep_string({ search = vim.fn.input("Grep > ") });
+        end,
+        "Telescope: grep"
+    },
+
+    -- nvim-tree
     ["<leader>ne"] = { ":NvimTreeFindFile<CR>", "Nvim-tree: find file in tree" },
     ["<leader>aa"] = { ":NvimTreeToggle<CR>", "Nvim-tree: toggle" },
 }
